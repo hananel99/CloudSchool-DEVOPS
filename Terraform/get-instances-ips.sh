a721f9718b6542de325506c8a5e8ecff892c9eaa
hananel99 <jbbtk99@gmail.com>
2023-10-16 16:25:46 +0300

    added sh script for getting ec2 names and public ips

---
+++ Terraform/get-instances-ips.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Install AWS CLI if not already installed (Uncomment the following line if needed)
+# pip install awscli
+
+# Configure your AWS CLI with the appropriate credentials
+# aws configure
+
+# Get a list of all running EC2 instances
+instance_ids=$(aws ec2 describe-instances --filters "Name=instance-state-name,Values=running" --query "Reservations[].Instances[].InstanceId" --output text)
+
+# Loop through the instance IDs and retrieve their names and public IP addresses
+for instance_id in $instance_ids; do
+    instance_name=$(aws ec2 describe-tags --filters "Name=resource-id,Values=$instance_id" "Name=key,Values=Name" --query "Tags[0].Value" --output text)
+    public_ip=$(aws ec2 describe-instances --instance-ids $instance_id --query "Reservations[].Instances[].PublicIpAddress" --output text)
+    
+    echo "Instance Name: $instance_name"
+    echo "Public IP: $public_ip"
+    echo
+done
