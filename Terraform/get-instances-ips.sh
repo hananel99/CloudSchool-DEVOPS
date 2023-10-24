d4485e6780f9ea68935c760b72d5c7b8c14c7d0c
hananel99 <hananel99@github.com>
2023-10-24 22:00:21 +0300

    Fixed chef app.conf.erb from extra lines

--- Terraform/get-instances-ips.sh
+++ Terraform/get-instances-ips.sh
@@ -13,8 +13,13 @@ instance_ids=$(aws ec2 describe-instances --filters "Name=instance-state-name,Va
 for instance_id in $instance_ids; do
     instance_name=$(aws ec2 describe-tags --filters "Name=resource-id,Values=$instance_id" "Name=key,Values=Name" --query "Tags[0].Value" --output text)
     public_ip=$(aws ec2 describe-instances --instance-ids $instance_id --query "Reservations[].Instances[].PublicIpAddress" --output text)
-    
+
     echo "Instance Name: $instance_name"
     echo "Public IP: $public_ip"
     echo
 done
+
+# Retrieve the DNS name of the Elastic Load Balancer
+elb_dns_name=$(aws elb describe-load-balancers --query "LoadBalancerDescriptions"[0]."DNSName" --output text)
+
+echo "Elastic Load Balancer DNS Name: $elb_dns_name"
